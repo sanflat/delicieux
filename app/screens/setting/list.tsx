4ade3def05f4551dc2f6759138207e1e5f28717d
sanflat <tradeforce0720@gmail.com>
2024-08-19 15:54:32 +0900

    Add links to your homepage and GitHub, closes #3

--- app/screens/setting/list.tsx
+++ app/screens/setting/list.tsx
@@ -1,5 +1,15 @@
 import React from "react";
-import { ScrollView, Box, HStack, useColorMode } from "native-base";
+import {
+  ScrollView,
+  Box,
+  HStack,
+  useColorMode,
+  Heading,
+  Text,
+  Avatar,
+  Button,
+} from "native-base";
+import * as Linking from 'expo-linking';
 import DisplayView from "@/components/layout/DisplayView";
 import MainArea from "@/components/layout/MainArea";
 import DefaultHeader from "@/components/common/header/DefaultHeader";
@@ -7,20 +17,25 @@ import SettingListView from "@/components/features/setting/SettingListView";
 import ThemeToggle from "@/components/common/theme/ThemeToggle";
 import { Setting } from "@/types/interface";
 import useAuth from "@/hooks/useAuth";
-import { useThemeTextColor, useThemeIconColor } from "@/hooks/useThemeColor";
+import { useThemeTextColor, useThemeIconColor, useThemeBtnColor } from "@/hooks/useThemeColor";
 import { ThemedText } from "@/components/common/text/ThemedText";
 import RippleButton from "@/components/common/button/RippleButton";
 
 export default function SettingListScreen() {
   const { signOut } = useAuth();
-  const { textPrimaryColor } = useThemeTextColor();
+  const { textPrimaryColor, borderColor } = useThemeTextColor();
   const { colorMode, toggleColorMode } = useColorMode();
   const { iconPrimaryColor } = useThemeIconColor();
+  const { btnBackground } = useThemeBtnColor();
 
   const settingsOptions: Setting[] = [
-    { title: "アカウント設定", icon: "account-edit", id: "account-setting" },
-    { title: "ログアウト", icon: "logout", func: () => signOut() },
+    { title: "Account", icon: "account-edit", id: "account-setting" },
+    { title: "Logout", icon: "logout", func: () => signOut() },
   ];
+
+  const openHomePage = () => {
+    Linking.openURL('https://yohei-kano.com');
+  }
 
   return (
     <DisplayView header={<DefaultHeader />}>
@@ -48,15 +63,34 @@ export default function SettingListScreen() {
               />
               <ThemedText
                 type="defaultSemiBold"
-                text={
-                  colorMode === "light"
-                    ? "Light mode"
-                    : "Dark mode"
-                }
+                text={colorMode === "light" ? "Light mode" : "Dark mode"}
                 color={textPrimaryColor}
               />
             </HStack>
             <ThemeToggle />
+          </Box>
+          <Box alignItems="center" mt={9}>
+            <Avatar
+              source={require("../../../assets/my.jpg")}
+              size="xl"
+              borderRadius={100}
+              mb={6}
+              borderColor="secondary.500"
+              borderWidth={3}
+            />
+            <Heading mb={4} size="xl">
+              Yohei Kano 👋
+            </Heading>
+            <Text fontSize="md" w="full">
+              Hello! I'm a software engineer based in Japan. If you would like
+              to learn more about my expertise and projects, please visit my
+              homepage.
+            </Text>
+            <Button bgColor={btnBackground} onPress={openHomePage} borderRadius="10px" h="50px" borderWidth={1} borderColor={borderColor} mt={3}>
+              <Text color={textPrimaryColor} fontWeight="bold">
+                View Homepage
+              </Text>
+            </Button>
           </Box>
         </MainArea>
       </ScrollView>
--- components/common/button/LogoButton.tsx
+++ components/common/button/LogoButton.tsx
@@ -1,22 +1,21 @@
-import { useToast, Image } from "native-base";
+import { useToast, IconButton, Icon } from "native-base";
+import * as Linking from 'expo-linking';
 import Ripple from "react-native-material-ripple";
 import { Ionicons } from "@expo/vector-icons";
 import { useThemeIconColor } from "@/hooks/useThemeColor";
 
-export default function LogoButton() {
+export default function SourceViewButton() {
   const { iconPrimaryColor } = useThemeIconColor();
-  const toast = useToast();
+  const openSourcePublicPage = () => {
+    Linking.openURL('https://github.com/sanflat/delicieux');
+  }
 
   return (
-    <Ripple rippleColor={iconPrimaryColor} rippleCentered onPress={() => toast.show({
-      description: "Welcome Delicieux"
-    })}>
-      {/* <Image
-        size="40px"
-        borderRadius="full"
-        source={require("@/assets/delisux.png")}
-        alt="App Logo"
-      /> */}
+    <Ripple rippleColor={iconPrimaryColor} rippleCentered onPress={openSourcePublicPage}>
+      <IconButton
+        icon={<Icon as={Ionicons} name="logo-github" size="xl" color={iconPrimaryColor} />}
+        background="transparent"
+      />
     </Ripple>
   );
 }
